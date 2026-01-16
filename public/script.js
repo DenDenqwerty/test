75549fb00ca706438434ab1a38db6f7dd3226df5
DenDenqwerty <dbrakhnov@gmail.com>
2026-01-16 16:48:51 +0700

    taktak4

--- public/script.js
+++ public/script.js
@@ -116,7 +116,12 @@ function connectToNewUser(newUserId, stream) {
 function createPeerConnection(peerId) {
   const pc = new RTCPeerConnection({
     iceServers: [
-      { urls: 'stun:stun.l.google.com:19302' }
+      { urls: 'stun:stun.l.google.com:19302' },
+      {
+        urls: 'turn:turn.bistri.com:80',
+        username: 'homeo',
+        credential: 'homeo'
+      }
     ]
   });
 
